c574c8e26d9e14d1374729b8a10e0f9edcc608d3
krishsah01 <157822205+krishsah01@users.noreply.github.com>
2024-02-12 16:05:06 -0500

    Commit

--- src/TheaterKiosk.java
+++ src/TheaterKiosk.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
 public class TheaterKiosk {
     public static void main(String[] args)
     {
+        int legal_age = 21;
+
+        int age;
+        System.out.print("Enter your age: ");
+        Scanner in = new Scanner(System.in);
+        age = in.nextInt();
+        in.nextLine();
+
+        if(age >= 21){
+            System.out.print("You get a Wrist Band");
+        }
 
     }
 }
